349c4ee683223add9b470e98a4001966df1f517b
AVG <valerogracia@me.com>
2016-06-03 11:20:56 +0200

    now with lenght output

--- dnacalc.py
+++ dnacalc.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
-# This is a comment
+# env is used to take python wherever we are
+# 'Python dosen't care about cases, but we have to be consistent!
 
 DNASeq = 'ATGAAC'
 
-print ( 'Sequence ' + DNASeq )
+print( 'Sequence ' + DNASeq )
+
+SeqLength = len( DNASeq )
+
+print( SeqLength )
